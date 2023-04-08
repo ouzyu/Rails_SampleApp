061d05982709054ed5a77b7095582aea5e0d522f
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-04-08 09:20:15 +0000

    [Finish}4-5,学習終了

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get 'lists/:id' => 'lists#show'
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
