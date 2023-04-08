12ed8010aa8f8252bfc70d41279ff09d97ba5aae
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-04-08 09:02:35 +0000

    [Start]4-5,学習開始

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/:id' => 'lists#show'
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
 end
